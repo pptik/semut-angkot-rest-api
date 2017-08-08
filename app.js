81cc102f2083230ff7236c2713f6a0cb5d375cc0
hynra <hynra@yahoo.com>
2017-08-08 11:56:21 +0700

    change lang level to ecma 6

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+let index = require('./routes/index');
+let users = require('./routes/users');
 
-var app = express();
+const app = express();
+const mongoConnect = require('./database/mongo_connection');
+async function connect() {
+    try{
+        let database = await mongoConnect.connect();
+    }catch (err){
+        console.log(err);
+    }
+}
+connect();
 
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,22 +29,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+
+app.use((req, res) => {
+  res.send(200).send({success: false, message: "Forbidden route"});
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+
+app.use((err, req, res) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.send(200).send({success: false, message: "Forbidden route"});
 });
 
 module.exports = app;
