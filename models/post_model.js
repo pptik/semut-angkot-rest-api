1339ecbb0527d9d0240b9b48a6f557dcd399bd37
hynra <hynra@yahoo.com>
2017-08-14 21:45:10 +0700

    post model

---
+++ models/post_model.js
@@ -0,0 +1,21 @@
+const app = require('../app');
+const moment = require('moment');
+let database = app.db;
+let postCollection = database.collection('tb_post_angkot');
+
+/** buat laporan **/
+createPost = (query) =>{
+  return new Promise(async(resolve, reject) =>{
+      try{
+          let result = await postCollection.insertOne(query);
+          resolve(result);
+      }catch (err) {
+          reject(err);
+      }
+  });
+};
+
+
+module.exports = {
+    createPost:createPost
+};
