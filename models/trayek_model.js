c285788951a76ac76d2f31a1eabb270ed2a061c2
hynra <hynra@yahoo.com>
2017-08-22 13:20:00 +0700

    tambah jumlah penumpang

---
+++ models/trayek_model.js
@@ -0,0 +1,30 @@
+const app = require('../app');
+const moment = require('moment');
+let database = app.db;
+let trayekCollection = database.collection('tb_angkot_trayek');
+
+/** tambah penumpang **/
+tambahPenumpang = (query) => {
+    return new Promise((resolve, reject) =>{
+        try{
+            let result = database.updateOne(
+                {
+                    "TrayekID" : parseInt(query['trayek_id']),
+                    "Arah.Flag" : parseInt(query['flag'])
+                },
+                {
+                    $inc : {
+                        "Arah.$.JumlahPenunggu" : parseInt(query('jumlah_penunggu'))
+                    }
+                }
+            );
+            resolve(result);
+        }catch (err){
+            reject(err);
+        }
+    });
+};
+
+module.exports = {
+    tambahPenumpang:tambahPenumpang
+};
--- routes/trayek.js
+++ routes/trayek.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const usrModel = require('../models/user_model');
+const trayekModel = require('../models/trayek_model');
 const commonMsg = require('../configs/common_messages.json');
 
 router.post('/tambah-penumpang', async(req, res) => {
@@ -11,7 +12,17 @@ router.post('/tambah-penumpang', async(req, res) => {
     if(session_id === undefined || jumlahPenunggu === undefined || trayekID === undefined || flag === undefined){
         res.status(200).send(commonMsg.body_body_empty);
     }else {
-
+        try {
+            let profile = await usrModel.checkSession(session_id);
+            if(profile === null) res.status(200).send(commonMsg.session_invalid);
+            else {
+                await trayekModel.tambahPenumpang(req.body);
+                res.status(200).send({success: true, code : '000', message: 'Berhasil menambahkan penunggu'});
+            }
+        }catch (err){
+            console.log(err);
+            res.status(200).send(commonMsg.service_not_responding);
+        }
     }
 });
 
