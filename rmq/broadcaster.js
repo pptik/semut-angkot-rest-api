7ee4855a8a24d7ce4c910b6a298e507e65a0c62d
hynra <hynra@yahoo.com>
2017-11-21 17:37:52 +0700

    return converted time

--- rmq/broadcaster.js
+++ rmq/broadcaster.js
@@ -1,13 +1,14 @@
 const userModel = require('../models/user_model');
 const tmdModels = require('../models/tmb_model');
 const configs = require('../configs/rmq.json');
+const converter = require('../utilities/converter');
 
 /** broadcast lokasi angkot **/
 broadcastAngkot = async(connection) => {
     try {
         let ch = await connection.createChannel();
         await ch.assertExchange(configs.exchange_name, 'topic', {durable: false});
-        let q = await ch.assertQueue(configs.broadcast_queue_name, {exclusive: false, messageTtl: 1000});
+        let q = await ch.assertQueue(configs.broadcast_queue_name, {exclusive: false, messageTtl: 2000});
         await ch.bindQueue(q.queue, configs.exchange_name, configs.broadcast_queue_name);
         console.log("starting broadcast via "+configs.broadcast_route);
         setInterval(async function () {
@@ -15,6 +16,10 @@ broadcastAngkot = async(connection) => {
             let dataAngkot = await userModel.getAngkotLocation();
           //  let dataPost = await postModel.getPosts();
             let dataTmb = await tmdModels.getTmbLocation();
+            for(let i = 0; i < dataTmb.length; i++){
+                dataTmb[i]['time'] = converter.convertISODateToString(new Date(dataTmb[i]['gpsdatetime']))
+            }
+
             let msg = {angkot : dataAngkot, laporan : [], tmb: dataTmb};
             msg = JSON.stringify(msg);
             await ch.publish(configs.exchange_name, configs.broadcast_route, new Buffer(msg));
