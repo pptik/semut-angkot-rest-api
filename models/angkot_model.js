87899eec5506afe65ab28da391488a5a6ad9c304
fiyyanputra <hafiyyan.jtk10@gmail.com>
2018-01-30 10:39:29 +0700

    modified angkot model

--- models/angkot_model.js
+++ models/angkot_model.js
@@ -3,6 +3,18 @@ let database = app.db;
 let userCollection = database.collection('tb_user');
 const converter = require('../utilities/converter');
 
+//initialize postgre db
+const pgp = require('pg-promise')();
+const cn = {
+    host: 'localhost',
+    port: 5432,
+    database: 'my-database-name',
+    user: 'user-name',
+    password: 'user-password'
+};
+
+const db = pgp(cn);
+
 /** get angkot location**/
 getAngkot = () => {
     return new Promise((resolve, reject) => {
@@ -25,7 +37,22 @@ getAngkot = () => {
     });
 };
 
+getAngkotTracker = () => {
+	return new Promise((resolve, reject) => {
+		db.any('SELECT devices.name, positions.longitude, positions.latitude, positions.devicetime FROM positions INNER JOIN devices ON positions.id = devices.positionid')
+			.then(function(data) {
+				// success;
+				resolve(data);
+			})
+			.catch(function(error) {
+				// error;
+				reject(error);
+			});
+	});
+};
+
 
 module.exports = {
-    getAngkot:getAngkot
+    getAngkot:getAngkot,
+	getAngkotTracker:getAngkotTracker
 };
